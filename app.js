56fa825d6a65186f725b72bdd2101c45aa42d2ba
frxx222 <jayveeenobay040@gmail.com>
2024-02-16 20:38:15 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,28 +1,54 @@
 const Joi = require('joi');
 const express = require('express');
 const app = express();
+
+
 app.use(express.json());
 
 const typeOfRecipes = [
   {
     id: 1,
     title: 'Manggo Grahams',
+    categories: 'Dessert',
     ingredients: ['Mango', 'Graham', 'Milk'],
     instructions: 'Put that mango to graham',
   },
   {
     id: 2,
     title: 'Adobo',
+    categories: 'Dinner',
     ingredients: ['Chicken', 'Soy Sauce', 'Vinegar'],
     instructions: 'Put all ingredients together',
   },
   {
     id: 3,
-    title: 'Sinigang',
-    ingredients: ['Pork', 'Tomato', 'Tamarind', 'Vegetables'],
-    instructions: 'Boil the pork, after that add the other ingredients',
+    title: 'Scramble Egg',
+    categories: 'Breakfast',
+    ingredients: ['Egg', 'salt', 'oil', ],
+    instructions: 'open the egg the scramble it then cook it',
   }
 ];
+// users
+const users = [];
+// viewing users
+app.get('/api/users', (req, res) => {
+  res.send(users);
+});
+// user registration
+app.post('/api/registration', (req, res) => {
+  if(!req.body.name || req.body.name.length < 3){
+    return res.status(400).send('name is required and should be a minimum of 3 characters.');
+  }
+  const newUsers = {
+    id: users.length + 1,
+    name: req.body.name,
+    password: req.body.password,
+  };
+  users.push(newUsers);
+  
+  res.redirect('/api/recipes');
+});
+
 
 // Get all recipes
 app.get('/api/recipes', (req, res) => {
@@ -54,11 +80,13 @@ app.post('/api/recipes', (req, res) => {
   const newRecipe = {
     id: typeOfRecipes.length + 1,
     title: req.body.title,
+    categories: req.body.categories,
     ingredients: req.body.ingredients,
     instructions: req.body.instructions,
   };
   typeOfRecipes.push(newRecipe);
   res.send(newRecipe);
+  res.render(typeOfRecipes);
 });
 
 // Edit a recipe
@@ -68,6 +96,7 @@ app.put('/api/recipes/:id', (req, res) => {
     return res.status(404).send('The recipe with the given id was not found.');
 
   recipe.title = req.body.title;
+  recipe.categories = req.body.categories;
   recipe.ingredients = req.body.ingredients;
   recipe.instructions = req.body.instructions;
   res.send(recipe);
@@ -79,6 +108,7 @@ app.put('/api/recipes/title/:title', (req, res) => {
     return res.status(404).send('The recipe with the given title was not found.');
   
   recipe.title = req.body.title;
+  recipe.categories = req.body.categories;
   recipe.ingredients = req.body.ingredients;
   recipe.instructions = req.body.instructions;
   res.send(recipe);
